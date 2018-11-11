2ad80fced7ca8755237ec0da3f873c64ffb1aa9b
apple <apple@appledeMacBook-Pro.local>
2018-11-11 21:12:26 +0800

    mqtt test.

---
+++ mqtt/user.h
@@ -0,0 +1,8 @@
+//WIFI
+#define SSID        "gogogo"
+#define PASSWORD    "11223344"
+
+//ONENET
+#define DEVICEID   "503743391"
+#define DEVICETOKEN   "thisismqttdevtoken"
+#define PROJECTID "186720"
